77206f0d7bffaf1065183cad9dd80aa90e1c6695
tughluq <abdulla.erkin@hotmail.com>
2014-07-30 18:32:05 +0800

    polls completed

--- polls/tests.py
+++ polls/tests.py
@@ -5,7 +5,23 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+import datetime
+
 from django.test import TestCase
+
+from django.utils import timezone
+from polls.models import Poll
+
+# python manage.py test polls
+class PollMethodTests(TestCase):
+    def test_was_published_recently_with_future_poll(self):
+        """
+        was_published_recently() should return False for polls whose
+        pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
+        self.assertEqual(future_poll.was_published_recently(), False)
+
 
 
 class SimpleTest(TestCase):
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,12 @@
 # Create your views here.
 
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import Context, loader
-from polls.models import Poll
+from django.core.urlresolvers import reverse
+from polls.models import Poll, Choice
 
-
+'''
 def index_1(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
     #for pool in latest_poll_list: print pool.question
@@ -16,11 +16,14 @@ def index_1(request):
     })
     return HttpResponse(template.render(context))
     #return HttpResponse("Hello World. You\re at the poll index.")
+'''
 
+'''
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
     context = {'latest_poll_list': latest_poll_list}
     return render(request, 'polls/index.html', context)
+'''
 
 def detail(request, poll_id):
     '''try:
@@ -28,13 +31,30 @@ def detail(request, poll_id):
     except Poll.DoesNotExist:
         raise Http404
     return render(request, 'polls/detail.html', {'poll': poll})'''
+    
+    '''
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll': poll})
+    '''
     #return HttpResponse("You're looking at pool %s." % poll_id)
 
+'''
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll':poll})
+'''
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
